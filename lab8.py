31a356fa8e2bbb5f6b47392b776f7d3babb7aa3d
Cian Martyn <G00404885@atu.ie>
2023-12-07 00:24:45 +0000

    Plot Greyscale Image

--- lab8.py
+++ lab8.py
@@ -2,3 +2,27 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+img = cv2.imread('ATU.jpg',)
+
+
+# Load the image
+imgOrig = cv2.imread('ATU.jpg',)
+
+# Convert to grayscale
+imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
+
+# Convert original image to RGB for correct color rendering in Matplotlib
+imgOrigRGB = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2RGB)
+
+# Plotting
+plt.subplot(2, 1, 1)
+plt.imshow(imgOrigRGB)
+plt.title('Original')
+plt.xticks([]), plt.yticks([])
+
+plt.subplot(2, 1, 2)
+plt.imshow(imgGray, cmap='gray')
+plt.title('GrayScale')
+plt.xticks([]), plt.yticks([])
+
+plt.show()
