c012e6761c70e30f4ceadbffc190e1786bfb7100
Cian Martyn <G00404885@atu.ie>
2023-12-07 00:44:51 +0000

    2x2 subplot

--- lab8.py
+++ lab8.py
@@ -4,25 +4,41 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('ATU.jpg',)
 
-
 # Load the image
 imgOrig = cv2.imread('ATU.jpg',)
 
-# Convert to grayscale
-imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
+imgGrey = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
 
-# Convert original image to RGB for correct color rendering in Matplotlib
-imgOrigRGB = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2RGB)
+# Apply Gaussian Blur with different kernel sizes
+kernelSize1 = (3, 3)
+kernelSize2 = (13, 13)
+imgBlurred1 = cv2.GaussianBlur(imgGrey, kernelSize1, 0)
+imgBlurred2 = cv2.GaussianBlur(imgGrey, kernelSize2, 0)
 
-# Plotting
-plt.subplot(2, 1, 1)
-plt.imshow(imgOrigRGB)
-plt.title('Original')
+# Create a figure to display the images
+plt.figure(figsize=(10, 8))
+
+# Create subplot: 2 rows, 2 columns
+plt.subplot(2, 2, 1)  # Top-left
+plt.imshow(cv2.cvtColor(imgOrig, cv2.COLOR_BGR2RGB))
+plt.title('Original Image')
 plt.xticks([]), plt.yticks([])
 
-plt.subplot(2, 1, 2)
-plt.imshow(imgGray, cmap='gray')
-plt.title('GrayScale')
+plt.subplot(2, 2, 2)  # Top-right
+plt.imshow(cv2.cvtColor(imgGrey, cv2.COLOR_BGR2RGB), cmap='gray')
+plt.title('Greyscale Image')
 plt.xticks([]), plt.yticks([])
 
+plt.subplot(2, 2, 3)  # Bottom-left
+plt.imshow(cv2.cvtColor(imgBlurred1, cv2.COLOR_BGR2RGB))
+plt.title(f'Blurred with {kernelSize1} Kernel')
+plt.xticks([]), plt.yticks([])
+
+plt.subplot(2, 2, 4)  # Bottom-right
+plt.imshow(cv2.cvtColor(imgBlurred2, cv2.COLOR_BGR2RGB))
+plt.title(f'Blurred with {kernelSize2} Kernel')
+plt.xticks([]), plt.yticks([])
+
+# Display the plot
+plt.tight_layout()
 plt.show()
